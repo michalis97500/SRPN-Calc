b3745401de72bccb06ee3932bccebedcf8f087b6
Michalis Christou <Michalis.Christou@PerkinElmer.com>
2021-10-07 05:54:07 +0300

    Better comments

--- InputHandler.java
+++ InputHandler.java
@@ -1,69 +1,56 @@
 import java.io.*;
 
-//Begine by setting up this class to handle the user input
-public class InputHandler{
-  public InputHandler(){
-    //Let the user know we are ready for input
-    System.out.println("Started successfully");
+public class InputHandler {
+  public InputHandler() {
+    // Let the user know we are ready for input
+    System.out.println("You may now interact with the SRPN calculator");
   }
 
-  //By using the BufferedReader we will capture the input of the user
-  //as a string. 
-  public String ReadLine(){
+  public String ReadLine() {
     BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     String userin;
-    //Use try/cath as BufferedReader could throw IOException
-    try {
+    try { // Note : Handle exception
       userin = input.readLine();
-      //The user input is now found at "userin" string. We will parse this string
-      //to cleanString remove anything that could mess with our program
       return cleanString(userin);
-    } 
-    catch (IOException e) {
-      System.out.println("Error in reading line : " +e);
-      userin = " " ;
+    } catch (IOException e) {
+      System.out.println("Error in reading line : " + e);
+      userin = " ";
       return userin;
     }
   }
 
-  //characters NOT to remove
+  // characters NOT to remove
   public static String notChars = "[^dr\\=\\%\\^\\*\\+\\-\\d/\\s\\\\]";
 
-  /* It's very hard to consider everything the user might try to input. This method
-  will attempt to strip the string from anything that the user might enter but is not
-  actually usefull/accepted by the SRPN calculator. It basically formats the string in a
-  way that our doMath method (found in Main) works
-  */
+  /*
+   * It's very hard to consider everything the user might try to input. This
+   * method will attempt to strip the string from anything that the user might
+   * enter but is not actually usefull/accepted by the SRPN calculator. It
+   * basically formats the string in a way that our doMath method (found in Main)
+   * works
+   */
   private static String cleanString(String string) {
-    //user input of nothing or "". return a plain whitespace that doMath can handle by ignoring it
-    if(string == null || string.length()==0){
+    if (string == null || string.length() == 0) { // NullorEmpty string, send plain whitespace handled by doMath
       return " ";
-    }
-    //User has entered something
-    else{
-      //remove the comments first
-      String noComments = string.replaceAll("#.*?#", "");
-      //remove everything that we dont care about
-      String removeNotAllowed = noComments.replaceAll(notChars, "");
-      //remove extra whitespaces
-      String removeWhitespaces = removeNotAllowed.replaceAll("\\s+", " ");
-      if(removeWhitespaces == null || removeWhitespaces.length()==0){
+    } else {
+      String noComments = string.replaceAll("#.*?#", ""); // comments must be enclosed with #
+      String removeNotAllowed = noComments.replaceAll(notChars, "");// remove everything that we dont care about
+      String removeWhitespaces = removeNotAllowed.replaceAll("\\s+", " ");// remove extra whitespaces
+      if (removeWhitespaces == null || removeWhitespaces.length() == 0) {
         return " ";
-      }
-      else{
-        //return removeWhitespaces;
+      } else {
         return addWhitetoOperator(removeWhitespaces.trim());
       }
     }
   }
 
-  //This method is called to place a whitespace between user input to make
-  //sure everything reaches the math method in the same format
-  //1+1 == 1 + 1 
-  private static String addWhitetoOperator(String string){
+  // This method is called to place a whitespace between user input to make
+  // sure everything reaches the math method in the same format
+  // 1+1 == 1 + 1
+  private static String addWhitetoOperator(String string) {
     StringBuilder stringBuilder = new StringBuilder();
-    for(String character : string.split("")){
-      switch(character){
+    for (String character : string.split("")) {
+      switch (character) {
         case "+":
           stringBuilder.append(" + ");
           break;
--- Main.java
+++ Main.java
@@ -2,41 +2,43 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-  //Stack to store operands and operators
   public static Stack<Double> stack = new Stack<>();
-  //random number identifier,stack max size
+  // random number identifier,stack max size
   public static int n = 0, stackMaxSize = 24;
-  //array to hold "random" doubles
+  // array to hold "random" doubles
   public static double[] randomArray = new double[60];
-  /*I have found out that "r" in the SRPN given seems to be 
-  pseudorandom. It appears there is an array of 60 doubles that are used for
-  the random function.
-  */
+
+  /*
+   * I have found out that "r" in the SRPN given seems to be pseudorandom. It
+   * appears there is an array of 60 doubles that are used for the random
+   * function.
+   */
   public static double randomNumber(int _int) {
     return randomArray[_int];
   }
-  public static void populateArray() { //this method will populate an array with "random" numbers that the user might need
-    int i = 0;
+
+  public static void populateArray() { // this method will populate an array with "random" numbers that the user might
+                                       // need
+    int howManyLines = 0;
     try {
-      //Begin by checking that our file is compatible with the array that we have
+      // Run a counter on the file to check the lines = array, if so enter the values
+      // on each line into array
       File file = new File("random.txt");
       Scanner lineCount = new Scanner(file);
-      //simple counter
       while (lineCount.hasNextDouble()) {
-        i++;
-        double dummy = lineCount.nextDouble(); //without this running on repl.it causes endless loop
+        howManyLines++;
+        double iDoNothing = lineCount.nextDouble(); // without this running on repl.it causes endless loop
       }
       lineCount.close();
-      if (i != 60) {
+      if (howManyLines != 60) {
         System.out.println("File given is not compatible. Lines must be 60");
       }
-      if (i == 60) {
-        //file looks to be okay, write the doubles in the array
+      if (howManyLines == 60) {
         Scanner line = new Scanner(file);
         int y = 0;
         while (line.hasNextDouble()) {
-          Double data = line.nextDouble();
-          randomArray[y] = data;
+          Double valueInLine = line.nextDouble();
+          randomArray[y] = valueInLine;
           y++;
         }
         line.close();
@@ -46,91 +48,91 @@ class Main {
     }
   }
 
-  //DELETED : This method will determine if the result is saturated and if it is, will yield
-  //min/max int limit 
-  //REASON : casting a double to an int and back again saturates it
+  // DELETED METHOD -> : This method will determine if the result is saturated and
+  // if it is, will yield min/max int limit
+  // REASON : casting a double to an int and back again saturates it
 
-  /*Since all inputs are split by either a space or a "\n", the input of this function expects
-  that the string is formatted in that way. In the following method the string is broken 
-  split by whitespaces to a variable "oper" (for both operand and operator) using a loop. Within the loop the method will check what exactly is oper and determine what to do with it. 
-  */
+  /*
+   * Since all inputs are split by either a space or a "\n", the input of this
+   * function expects that the string is formatted in that way. In the following
+   * method the string is split by whitespaces to a variable "oper" (for both
+   * operand and operator) using a loop. Within the loop the method will check
+   * what exactly is oper and determine what to do with it.
+   */
   public static int doMath(String expr) {
-    //2ndary defense mechanism, if somehow a null, empty or plain whitespace string
-    //manages to get through, this is cancel the method
     if (expr == "" || expr == " ") {
       return 0;
-    }
+    } // Defense mechanism for null/empty input
     try {
-      //Split the string into the different operators/operands, loop for all
-      for (String oper: expr.split("\\s")) {
-        double number1 = 0.0 , number2 = 0.0;
-        //Statements that can be executed with stack size < 1:
-        //push number to stack, stack display, last answer,push random to stack
+      // Split the string into the different operators/operands, loop for all
+      for (String oper : expr.split("\\s")) {
+        double number1 = 0.0, number2 = 0.0;
+        // Statements that can be executed with stack size < 1:
+        // push number to stack, stack display, last answer,push random to stack
         if (oper.matches("-?\\d+(\\.\\d+)?") && oper.isEmpty() == false && oper.isBlank() == false) {
-          //many conditions to avoid crashishng when the user inputs a whitespace first
-          //i.e. " 1"
+          // many conditions to avoid crashishng when the user inputs a whitespace first
+          // i.e. " 1"
           pushDoubleToStack(stack, Double.parseDouble(oper));
         }
-        if (oper.matches("d")) {
+        if (oper.matches("d")) { // Display stack
           Object[] srpnArray = stack.toArray();
           for (int i = 0; i < srpnArray.length; i++) {
             System.out.format("%.0f\n", srpnArray[i]);
           }
         }
-        if (oper.matches("=")) {
+        if (oper.matches("=")) { // Show last answer
           double last = stack.pop();
           pushDoubleToStack(stack, last);
           System.out.println("Answer: " + last);
         }
-        if (oper.matches("r")) {
+        if (oper.matches("r")) { // Random
           if (n > 59) {
             n = 0;
           }
           pushDoubleToStack(stack, randomNumber(n));
           n++;
         }
-        //Check if the oper is an operator. While this is not strictly necessary,
-        //it is used to display stack underflow only when an operator is used
+        // Check if the oper is an operator. While this is not strictly necessary,
+        // it is used to display stack underflow only when an operator is used
         if (oper.matches("[\\+\\-\\*\\/\\^\\%]")) {
-          //if this is an operator, we require at least 2 operands so stack size must be > 1
-          if (stack.size() > 1) {
+          if (stack.size() > 1) { // Check enough operands exist
             switch (oper) {
-            case "+":
-              number2 = stack.pop();
-              number1 = stack.pop();
-              pushDoubleToStack(stack, number1 + number2);
-              break;
-            case "-":
-              number2 = stack.pop();
-              number1 = stack.pop();
-              pushDoubleToStack(stack, number1 - number2);
-              break;
-            case "*":
-              number2 = stack.pop();
-              number1 = stack.pop();
-              pushDoubleToStack(stack, number1 * number2);
-              break;
-            case "/":
-              number2 = stack.pop();
-              number1 = stack.pop();
-              if (number2 == 0.0) {
-                System.out.println("Cannot divide by zero!");
-                pushDoubleToStack(stack, number1);
-                pushDoubleToStack(stack, number2);
+              case "+":
+                number2 = stack.pop();
+                number1 = stack.pop();
+                pushDoubleToStack(stack, number1 + number2);
                 break;
-              }
-              pushDoubleToStack(stack, number1 / number2);
-              break;
-            case "^":
-              number2 = stack.pop();
-              number1 = stack.pop();
-              pushDoubleToStack(stack, Math.pow(number1, number2));
-              break;
-            case "%":
-              number2 = stack.pop();
-              number1 = stack.pop();
-              pushDoubleToStack(stack, number1 % number2);
-              break;
+              case "-":
+                number2 = stack.pop();
+                number1 = stack.pop();
+                pushDoubleToStack(stack, number1 - number2);
+                break;
+              case "*":
+                number2 = stack.pop();
+                number1 = stack.pop();
+                pushDoubleToStack(stack, number1 * number2);
+                break;
+              case "/":
+                number2 = stack.pop();
+                number1 = stack.pop();
+                if (number2 == 0.0) {
+                  System.out.println("Cannot divide by zero!");
+                  pushDoubleToStack(stack, number1);
+                  pushDoubleToStack(stack, number2);
+                  break;
+                }
+                pushDoubleToStack(stack, number1 / number2);
+                break;
+              case "^":
+                number2 = stack.pop();
+                number1 = stack.pop();
+                pushDoubleToStack(stack, Math.pow(number1, number2));
+                break;
+              case "%":
+                number2 = stack.pop();
+                number1 = stack.pop();
+                pushDoubleToStack(stack, number1 % number2);
+                break;
             }
           } else {
             System.out.println("Stack undeflow");
@@ -142,23 +144,20 @@ class Main {
     }
     return 1;
   }
-  //Method to write to stack (push), used to control stack overflow
-  public static void pushDoubleToStack(Stack < Double > stack, double number) {
+
+  public static void pushDoubleToStack(Stack<Double> stack, double number) {
     if (stack.size() >= stackMaxSize) {
       System.out.println("Stack Overflow");
-    }
-    //stack is not too large
-    else {
-      //saturate number if too large. By casting the double to an int it saturates it
-      stack.push((double)(int)number);
+    } else {
+      // saturate number if too large
+      stack.push((double) (int) number);
     }
   }
-  //Main function
+
   public static void main(String[] args) {
     try {
-      populateArray(); //fill our "random" array with doubles
-      InputHandler input = new InputHandler(); //Start an input handler class 
-      //Begin recieveing commands
+      populateArray(); // fill our "random" array
+      InputHandler input = new InputHandler(); // Start an input handler class
       while (true) {
         doMath(input.ReadLine());
       }
